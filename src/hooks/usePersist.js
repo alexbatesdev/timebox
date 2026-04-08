f5b0a4a1f827edb03b11461bc4697de658908f88
Alex Bates <alex.bates@builtapp.com>
2026-04-08 09:09:24 -0600

    Fix persistence bug

--- src/hooks/usePersist.js
+++ src/hooks/usePersist.js
@@ -1,9 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { todayKey } from "../utils/time.js";
 import { saveState } from "../utils/storage.js";
 
 export const usePersist = (schedType, blocks, tasks, wrapup, notionPageId) => {
+  const initKey = useRef(todayKey());
+
   useEffect(() => {
     if (schedType === null) return;
+    if (todayKey() !== initKey.current) return;
     saveState({ schedType, blocks, tasks, wrapup, notionPageId });
   }, [schedType, blocks, tasks, wrapup, notionPageId]);
 };
